a4af8e833488f7a940f86b233a5a366c2160ce65
Cedric Gatay <c.gatay@code-troopers.com>
2020-03-19 15:43:23 +0100

    feat(kotlin): added bidirectional communication
    Run Eklair in a background queue and added bidirectional communication between kotlin and swift.

--- Eklair/ContentView.swift
+++ Eklair/ContentView.swift
@@ -9,18 +9,51 @@
 import SwiftUI
 import eklair_node
 
+var counter = 0
+
 struct ContentView: View {
+    @State var message: String = ""
+    @State var hashedMessage: String = ""
+    
+    let user = EklairUser(id: Uuid().fromString(s: UUID().uuidString))
+    let logger = MessageLogger()
+    
+    
     var body: some View {
         VStack{
         Text("Hello, World!")
-            Button(action: { self.test() }){
-                Text("Run Eklair")
-            }
+        Button(action: { self.test() }){
+            Text("Run Eklair")
         }
+            TextField("Message", text: $message)
+            Button(action: { self.hashMe() }){
+                Text("Hash Message")
+            }
+            Text("Hashed value \(hashedMessage)")
+        }
+
+    }
+    
+    func hashMe(){
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        let msg = MessageContainer(message: formatter.string(for: Date())!, counter: Int32(counter), identity: user)
+        logger.log(msg: msg)
+        logger.nativeLog {
+            NSLog("Swifty logging")
+            return "Swift string \(type(of: self))"
+        }
+        counter += 1
+        hashedMessage = EklairDTO(message: message).hashValues()
+        
     }
     
     func test() {
-        Eklair.init().run()
+        let queue = DispatchQueue.init(label: "Eklair", qos: .background)
+        queue.async {
+            Eklair.init().run()
+        }
     }
 }
 
